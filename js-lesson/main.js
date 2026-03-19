c5d50ae5ee6baeaa7bb2e2e162b874cde59f163d
kiyo4810 <kiyo4810@gmail.com>
2026-03-19 21:52:00 +0900

    DOM操作他２

--- js-lesson/main.js
+++ js-lesson/main.js
@@ -1,63 +1,5 @@
 'use strict';
-console.log('hello from Pap');
-console.log('hello from Pap');
-console.log('hello from Pap');
-console.log('hello from Pap');
-let x = 4;
-console.log(x);
-x = x * 4;
-console.log(x);
-
-// エスケープシーケンス
-console.log("hello I'm Kiyo");
-console.log('\¥ \t AAA \n BBB');
-
-// インクリメント　デクリメント
-let num = 10;
-num++;
-console.log(num);
-
-++num;
-console.log(num);
-
---num;
-console.log(num);
-
-let num1 = 10;
-let num2 = 20;
-// ++してから代入
-let ans1 = ++num1;
-// 代入してから++
-let ans2 = num2++;
-
-console.log(ans1, ans2);
-
-let valX = 60;
-if (valX >= 100) {
-  console.log('100以上の値です');
-} else {
-  console.log('100未満の値です');
-}
-
-if (valX >= 90) {
-  console.log('A');
-} else if (valX >= 80) {
-  console.log('B');
-} else if (valX >= 60) {
-  console.log('C');
-  // }else if (valX<60) {
-} else {
-  console.log('D');
-}
-let numnum = 100;
-let numstr = '100';
-if (numnum === numstr) {
-  console.log('同じ');
-} else {
-  console.log('別物');
-}
-
-// 「ここから処理を開始せよ」という「入り口（ラベル）」に過ぎない。
+// caseは「ここから処理を開始せよ」という「入り口（ラベル）」に過ぎない。
 // その直後にある break; という看板を見て、ようやく「あ、ここで終わりか」と判断して外に出ます。
 switch (1) {
   case 1:
@@ -250,13 +192,37 @@ btn.addEventListener('click', (e) => {
   const textIn = document.getElementById('textInput');
   const Ullist = document.getElementById('list');
   const li = document.createElement('li');
-  li.innerText = textIn.value;
-  // Ullist.appendChild(li);
-  Ullist.prepend(li);
-  //おまけ。追加後入力フォームをからにする
-  textIn.value = '';
-  // 3. 【ここがポイント】透かし文字を書き換える
-  textIn.placeholder = '次、どんどん入力してね';
+
+  if (textIn.value !== '') {
+    li.innerText = textIn.value;
+    // Ullist.appendChild(li);
+    Ullist.prepend(li);
+    //おまけ。追加後入力フォームをからにする
+    textIn.value = '';
+    // 3. 【ここがポイント】透かし文字を書き換える
+    textIn.placeholder = '次、どんどん入力してね';
+  }
+});
+
+// ノードの置き換え
+const elemB = document.getElementById('button2');
+elemB.addEventListener('click', () => {
+  const newList = document.createElement('li');
+  newList.setAttribute('id', 'newList');
+  const newText = document.createTextNode('新しい要素');
+  newList.appendChild(newText);
+  const oldList = document.getElementById('oldList');
+  const parentNode = oldList.parentNode;
+  parentNode.replaceChild(newList, oldList);
+});
+
+// ノードの削除
+const elemB2 = document.getElementById('button3');
+elemB2.addEventListener('click', () => {
+  const parentElement = document.getElementById('list3');
+  const elements = parentElement.getElementsByTagName('li');
+  const removeIndex = elements.length - 1;
+  parentElement.removeChild(elements[removeIndex]);
 });
 
 /////////////////////////////////////////////
